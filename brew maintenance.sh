d01db6d7feb35086b5755a273c41432075f060fc
Luis Puerto <luiss.puerto@gmail.com>
2018-09-20 23:18:43 +0300

    add new file

---
+++ brew maintenance.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# In this script we are going to update, upgrade, pu
+#
+
+# brew update && brew upgrade && brew cleanup && brew prune && brew cask outdated && brew cask cleanup
+
+set -x
+brew list
+set +x
+echo "get into list"
+brew list > ~/Coding/Brewing/brew\ apps.txt
+brew cask list > ~/Coding/Brewing/brew\ cask\ apps.txt
+
+$SHELL
