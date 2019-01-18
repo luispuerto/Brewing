68b621a7f00d3f76c56d86005cd27939f7e00f2a
Luis Puerto <luiss.puerto@gmail.com>
2019-01-18 09:04:31 +0200

    Disable the R install part.

--- install-qgis3+r.sh
+++ install-qgis3+r.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-set -v # verbose, to get the commands in the shell, not just the output. 
+set -v # verbose, to echo the commands in the shell, not just the output. 
 brew uninstall qgis3 
 brew unlink pyqt-qt4
-brew install qgis3 --with-grass --with-saga-gis-lts --with-r 
+brew install qgis3 --with-grass --with-saga --with-r --with-orfeo6
+# brew install qgis3 --with-grass --with-saga --with-r 
 brew link pyqt-qt4
-brew uninstall --ignore-dependencies r 
-brew uninstall --ignore-dependencies cairo
-brew install sethrfore/r-srf/cairo
-brew install sethrfore/r-srf/r --with-openblas --with-java --with-libtiff --with-cairo
+# brew uninstall --ignore-dependencies r 
+# brew uninstall --ignore-dependencies cairo
+# brew install sethrfore/r-srf/cairo
+# brew install sethrfore/r-srf/r --with-openblas --with-java --with-libtiff --with-cairo
+rm /Applications/QGIS.app
+mv /usr/local/Cellar/qgis3/3.4.3_2/QGIS.app /Applications
+ln -s /Applications/QGIS.app /usr/local/Cellar/qgis3/3.4.3_2/QGIS.app
